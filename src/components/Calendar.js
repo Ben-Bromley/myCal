6660f6e42a63045244e4b5e4c439c67fd2bcdb71
Ben (Macbook) <benbromley.104@gmail.com>
2021-06-07 15:47:39 +0100

    basic table generation complete,
    previously large constructor method now divided into other methods called in render method

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -6,22 +6,90 @@ class Calendar extends React.Component {
 
 	constructor(props) {
 		super(props)
-		this.state = {dateObject: moment()}
+		// this.state = {dateObject: moment()}
+		// generates days of the week as th elements
 
+
+		// firstDayOfMonth() => {
+		// 	let dateObject = this.state.dateObject;
+		// 	let firstDay = moment(dateObject).startOf('month').format.'d';
+		// }
+
+	};
+
+	addHeaders() {
 		this.weekdayShort = moment.weekdaysShort();
 		this.weekDaysShortName = this.weekdayShort.map(day => {
-			return (
-				<th key={day} className="week-day">{day}</th>
-			);
+			return <th key={day} className="week-day">{day}</th>;
 		});
-	};
+		return this.weekDaysShortName;
+	}
+
+	createTimeSlots() {
+		let x = {
+		  slotInterval: 30,
+		  openTime: '0:00',
+	  	  closeTime: '24:00'
+		};
+
+		//Format the time
+		let startTime = moment(x.openTime, "HH:mm");
+		//Format the end time
+		let endTime = moment(x.closeTime, "HH:mm")
+		//Times
+		let allTimes = [];
+		//Loop over the times - only pushes time with 30 minutes interval
+		while (startTime < endTime) {
+		  //Push times
+		  allTimes.push(startTime.format("HH:mm")); 
+		  //Add interval of 30 minutes
+		  startTime.add(x.slotInterval, 'minutes');
+		}
+		console.log(allTimes);
+		return allTimes;
+	}
+
+	addTimesToTable() {
+		this.timeSlots = this.createTimeSlots();
+		this.timeSlotElements = [];
+		for (let d = 0; d < 48; d++){
+			this.timeSlotElements.push(
+				<tr key={d} className="timeSlot">
+					<td>{this.timeSlots[d]}</td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+			);
+		}
+		return this.timeSlotElements;
+	}
+
 	render() {
 		return (
 			<div id="cal-container">
 				<h2>My Weekly Agenda</h2>
 				<table>
-					<tr>{this.weekDaysShortName}</tr>
-					<tr></tr>
+					<thead>
+						<tr><th>(Times)</th>{this.addHeaders()}</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>All Day</td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+						</tr>
+						{this.addTimesToTable()}
+					</tbody>
 				</table>
 			</div>
 		);
