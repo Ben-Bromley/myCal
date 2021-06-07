6a49d68f73886a502c0f8ee0ea05d2167253ef21
Ben (Macbook) <benbromley.104@gmail.com>
2021-06-07 12:17:08 +0100

    added firebase config

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBek52EGDWF7YEakz_aB-6SSpjjavA1_7s",
+  authDomain: "my-cal-76221.firebaseapp.com",
+  projectId: "my-cal-76221",
+  storageBucket: "my-cal-76221.appspot.com",
+  messagingSenderId: "925326808080",
+  appId: "1:925326808080:web:3f0f75a9b2c8da6b6b6695"
+};
+
+
+firebase.initializeApp(config);
+export default firebase;
