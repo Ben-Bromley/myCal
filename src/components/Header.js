abfda1b956d5017ec284a6685617c7ad52cf0b0d
Ben (Macbook) <benbromley.104@gmail.com>
2021-06-07 13:48:22 +0100

    added commented out function components for referencing

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,3 +23,12 @@ const Header = () => {
 }
 
 export default Header;
+
+// function Welcome(props) {  return <h1>Hello, {props.name}</h1>;
+// }
+
+// const element = <Welcome name="Sara" />;ReactDOM.render(
+//   element,
+//   document.getElementById('root')
+// );
+
