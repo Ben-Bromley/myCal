192a6aa270eccad6ed7b6e002603952c7f839aac
Ben (Macbook) <benbromley.104@gmail.com>
2021-06-07 20:30:04 +0100

    finished UI aspect of component, added basic functions

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,31 +1,61 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
 import moment from 'moment'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleLeft } from '@fortawesome/free-solid-svg-icons'
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
 
 class Calendar extends React.Component {
-
 	constructor(props) {
 		super(props)
-		// this.state = {dateObject: moment()}
-		// generates days of the week as th elements
+		this.state = {
+			// dateObject: moment(),
+			currentmonth: moment(),
+			month: moment().format("MMMM"),
+			today: moment(),
+			weekStart: moment().startOf('week').format("M/D"),
+			weekEnd: moment().endOf('week').format("M/D"),
 
-
-		// firstDayOfMonth() => {
-		// 	let dateObject = this.state.dateObject;
-		// 	let firstDay = moment(dateObject).startOf('month').format.'d';
-		// }
-
+		};
+		// this.weekStart = moment().startOf('week').format("M/D")
+		// this.weekEnd = moment().endOf('week').format("M/D")
+		this.weekStart = moment().startOf('week')
+		this.weekEnd = moment().endOf('week')
 	};
 
+	getHeaderDates() {
+		// initalises array
+		const weekDates = [];
+		// adds all dates for the week to array
+		for (let i = 0; i < 7; i++){
+		  //Push day
+		  weekDates.push(this.weekStart.format('M/D')); 
+		  //Adds a day
+		  this.weekStart.add(1, 'days');
+		};
+		// returns list of dates for the work
+		return weekDates;
+	}
+
 	addHeaders() {
+
+		// pulls header names from other method
 		this.weekdayShort = moment.weekdaysShort();
-		this.weekDaysShortName = this.weekdayShort.map(day => {
-			return <th key={day} className="week-day">{day}</th>;
+		// pulls header dates from other method
+		this.dateArray = this.getHeaderDates();
+		// matches date and weekday to match in array
+		this.weekDaysShortName = this.weekdayShort.map((day, index) => {
+			return (
+				<th key={day} className="week-day">
+				{day}<br />{this.dateArray[index]}
+				</th>
+			);
 		});
 		return this.weekDaysShortName;
 	}
 
 	createTimeSlots() {
+
 		let x = {
 		  slotInterval: 30,
 		  openTime: '0:00',
@@ -38,14 +68,14 @@ class Calendar extends React.Component {
 		let endTime = moment(x.closeTime, "HH:mm")
 		//Times
 		let allTimes = [];
-		//Loop over the times - only pushes time with 30 minutes interval
+		//Loops over times - pushes times with 30 mins interval
 		while (startTime < endTime) {
 		  //Push times
 		  allTimes.push(startTime.format("HH:mm")); 
 		  //Add interval of 30 minutes
 		  startTime.add(x.slotInterval, 'minutes');
 		}
-		console.log(allTimes);
+		// console.log(allTimes);
 		return allTimes;
 	}
 
@@ -69,10 +99,27 @@ class Calendar extends React.Component {
 		return this.timeSlotElements;
 	}
 
+	prevMonth() {
+		console.log('previous clicked!')
+		// this.setState({month: })
+	};
+	nextMonth() {
+		console.log('next clicked!')
+	};
+
 	render() {
 		return (
 			<div id="cal-container">
-				<h2>My Weekly Agenda</h2>
+				<div id="header-container">
+					<h2>My Weekly Agenda</h2>
+					<div id="month-selector-container">
+						<FontAwesomeIcon icon={faAngleLeft}id="prev-icon"
+						onClick={() => this.prevMonth()}/>
+						<span>{this.state.month}</span>
+						<FontAwesomeIcon icon={faAngleRight} id="next-icon"
+						onClick={() => this.nextMonth()}/>
+					</div>
+				</div>
 				<table>
 					<thead>
 						<tr><th>(Times)</th>{this.addHeaders()}</tr>
@@ -93,7 +140,7 @@ class Calendar extends React.Component {
 				</table>
 			</div>
 		);
-	}
-}
+	};
+};
 
 export default Calendar;
