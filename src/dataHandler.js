d417f6c2a73aa47b0a52b55b7e216008a367b7a9
Ben (Macbook) <benbromley.104@gmail.com>
2021-06-18 22:13:31 +0100

    getEvents( progressed )
    can now access all elements that have isAllDay == false

--- src/dataHandler.js
+++ src/dataHandler.js
@@ -1,8 +1,9 @@
 class dataHandler {
     // for getting the events that last all day
-    static async getWeekEvents(dbRef, dates) {
+    static async getAllDayEvents(dbRef, dates) {
         // gets all day events matching current dates
         let allDayWeekEvents = dbRef.where('isAllDay', '==', true).where('date', 'in', dates)
+        // console.log('allDayWeekEvents => ', allDayWeekEvents)
         let response = await allDayWeekEvents.get()
         let documents = []
         response.forEach(doc => documents.push(doc.data()));
@@ -15,6 +16,35 @@ class dataHandler {
         }
         return eventArray;
     };
+
+    static async getEvents(dbRef, dates) {
+        // TODO: make sure all normal events have isAllDay set to false!!
+        // gets events from current week for during the day
+        // console.log(dates)
+        let weekEvents = dbRef.where('isAllDay', '==', false).where('date', 'in', dates)
+        // console.log('query => ', weekEvents)
+        let response = await weekEvents.get();
+        console.log('response =>', response)
+        response.forEach(doc => console.log('doc data => ', doc.data()))
+
+
+        /*
+
+        [
+            [ data for 00:00 ],
+            [ data for 00:30 ],
+            [ data for 01:00 ],
+            [ data for 01:30 ],
+            [ data for 02:00 ],
+            [ data for 02:30 ],
+            etc...
+        ]
+
+        */
+
+
+    }
+
     // for using the event inspector / popup
     static getEventObjectFromName() {};
 };
